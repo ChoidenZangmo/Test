0f4494a2424fd0c34f4b984d246265820e9328c6
unknown <02230061.cst@rub.edu.bt>
2024-02-29 11:54:53 +0600

    29th feb classwork

---
+++ 29th classwork.py
@@ -0,0 +1,20 @@
+name = input("Enter your Name:")
+print(name)
+
+
+num1 =  float(input("Enter your first digit:"))
+num2 =  float(input("Enter your second digit:"))
+a = num1 + num2 
+print(f"addition_result = {a}")
+
+s = num1 - num2
+print(f"subtraction_result = {s}")
+
+m = num1 * num2
+print(f"multiplication_result = {m}")
+
+d = num1 / num2
+print(f"division_result = {d}")
+
+
+
